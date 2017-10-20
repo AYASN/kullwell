870534a2687b04700c63835cf757f72cbd509e9a
AYassine <assiryassine@gmail.com>
2017-10-20 23:04:48 +0200

    WIP: Add DB connection

--- src/main/java/com/yandm/assir/dao/JDBCConnection.java
+++ src/main/java/com/yandm/assir/dao/JDBCConnection.java
@@ -6,12 +6,13 @@ import java.sql.SQLException;
 
 public class JDBCConnection {
 
-    private static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
-    private static JDBCConnection instance = new JDBCConnection();
+    private static final java.lang.String DRIVER_CLASS = "org.sqlite.JDBC";
+    String urlToDbFile = this.getClass().getClassLoader().getResource("kullWell.db").toString();
     public static String DB_URL = null;
-
+    private static JDBCConnection instance = new JDBCConnection();
 
     public JDBCConnection() {
+        DB_URL = "jdbc:sqlite" + urlToDbFile;
 
         try {
             Class.forName(DRIVER_CLASS);
@@ -22,10 +23,10 @@ public class JDBCConnection {
 
     private Connection createConnection() {
 
-        DB_URL = "jdbc:mysql://localhost:3306/";
+//        DB_URL = "jdbc:mysql://localhost:3306/";
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection(DB_URL, "root", "");
+            connection = DriverManager.getConnection(DB_URL);
         } catch (SQLException e) {
             System.err.println("ERROR: Unable to connect to Database.");
         }
