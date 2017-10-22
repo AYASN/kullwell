237c7c348d1cdbe853cd96bbc5ffeb56988cdd3d
AYassine <assiryassine@gmail.com>
2017-10-22 10:20:13 +0200

    Add Calories and Season to the Ingredients

--- src/main/java/com/yandm/assir/model/Ingredient.java
+++ src/main/java/com/yandm/assir/model/Ingredient.java
@@ -2,22 +2,43 @@ package com.yandm.assir.model;
 
 public class Ingredient {
 
-    private Long id;
-    private String name;
+   private Long id;
+   private String name;
 
-    public Long getId() {
-        return id;
-    }
+   private int calories;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+   private String season;
 
-    public String getName() {
-        return name;
-    }
+   public Long getId() {
+      return id;
+   }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public int getCalories() {
+      return calories;
+   }
+
+   public void setCalories(int calories) {
+      this.calories = calories;
+   }
+
+   public String getSeason() {
+      return season;
+   }
+
+   public void setSeason(String season) {
+      this.season = season;
+   }
 }
